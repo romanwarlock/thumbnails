ddce0fdc9440a78b755f76428dce4dad88b2776e
romanwarlock <roman.warlock@gmail.com>
2022-07-20 16:12:45 +0300

    bc & ImageMagick free

--- thumbgen.sh
+++ thumbgen.sh
@@ -2,11 +2,11 @@
 # 
 #
 # Dependencies/Requirements:
-# 1. ffmpeg
+# 1. ffmpeg 
 # 2. awk
 # 3. sed
-# 2.72 Test version - removing bc (basic calculator)
-# ImageMagick dependence is REMOVED
+# bc dependence has been REMOVED - awk is used for calculations
+# ImageMagick dependence has been REMOVED - ffmpeg is used to work with jpg-files
 # 
 # Usage:
 # . th.sh COLUMNS ROWS SIZE INPUT
@@ -20,13 +20,17 @@
 # Example:
 # . thumbgen.sh 3 14 1920 video.mp4
 # . <path-to-file>/thumbgen.sh 3 14 1920 video.mp4
-
-#if ffmpeg is not installed via snap / apt-get / etc.../ I just downloaded already compiled:
-# ffmpeg can be set as ffmpeg="/media/rk/0/soft/ffmpeg-4.4-amd64-static/ffmpeg" and further
-#the command "ffmpeg" shall be replaced with the variable "$ffmpeg"
-
+#
+#if $ffmpeg  is not installed via snap / apt-get / etc.../ I just downloaded already compiled:
+# $ffmpeg  can be set as $ffmpeg ="/media/rk/0/soft/$ffmpeg -4.4-amd64-static/$ffmpeg " and further
+#the command "$ffmpeg " shall be replaced with the variable "$$ffmpeg "
+#
 #if path to file or filename has special characters (space, etc) use "":
 # . thumbgen.sh 3 14 1920 "/wh ateve R/v (i) {deo.mp4"
+
+ffmpeg="/media/rk/0/soft/ffmpeg/ffmpeg"
+
+
 if [[ $# != 4 ]]; then
     echo "wrong number of arguments
 
@@ -49,12 +53,12 @@ X=$1
 Y=$2
 NFRAMES=`echo | awk '{print (X*Y)}' X="$X" Y="$Y}}"`
 #NFRAMES=$(echo "scale=0;$1*$2" | bc)
-TILE=$(echo "$Xx$Y")
+TILE=$(echo "${X}x${Y}")
 SIZE=$3
 INPUT=$4
-DURX=$(ffmpeg -i "$4" 2>&1 | grep Duration | awk '{print $2}' | tr -d ,)
-DURATION=$(ffmpeg -i "$4" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }')
-RES=$(ffmpeg -i "$4" 2>&1 | grep -oP 'Stream .*, \K[0-9]+x[0-9]+')
+DURX=$($ffmpeg  -i "$4" 2>&1 | grep Duration | awk '{print $2}' | tr -d ,)
+DURATION=$($ffmpeg  -i "$4" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }')
+RES=$($ffmpeg  -i "$4" 2>&1 | grep -oP 'Stream .*, \K[0-9]+x[0-9]+')
 FILESIZE=$(du -sm "$4" | awk '{print $1}')
 TMPDIR=/tmp/thumbnails-${RANDOM}/
 mkdir $TMPDIR
@@ -73,18 +77,19 @@ if [ $VARIABLE -gt 9 ];then
 else
   ZEROS="000"
 fi
-ffmpeg -start_at_zero -copyts -ss $OFFSET -i "$4" -vf "drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:fontsize=45:fontcolor=white::shadowcolor=black:shadowx=2:shadowy=2:box=1:boxcolor=black@0:x=(W-tw)/40:y=H-th-10:text='%{pts\:gmtime\:0\:%H\\\\\\:%M\\\\\:%S}'" -vframes 1 ${TMPDIR}$ZEROS$VARIABLE.jpeg
+$ffmpeg  -start_at_zero -copyts -ss $OFFSET -i "$4" -vf "drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:fontsize=45:fontcolor=white::shadowcolor=black:shadowx=2:shadowy=2:box=1:boxcolor=black@0:x=(W-tw)/40:y=H-th-10:text='%{pts\:gmtime\:0\:%H\\\\\\:%M\\\\\:%S}'" -vframes 1 ${TMPDIR}$ZEROS$VARIABLE.jpeg
 done
-ffmpeg -pattern_type glob -i "${TMPDIR}*.jpeg" -filter_complex tile=$TILE:margin=4:padding=4:color=white ${TMPDIR}output.jpg
+$ffmpeg  -pattern_type glob -i "${TMPDIR}*.jpeg" -filter_complex tile=$TILE:margin=4:padding=4:color=white ${TMPDIR}output.jpg
 echo "  $4" >>${TMPDIR}myfile.txt
 echo "  $RES" >>${TMPDIR}myfile.txt
 echo "  $FILESIZE Mb" >>${TMPDIR}myfile.txt
 echo "  $DURX" >>${TMPDIR}myfile.txt
-thewidth=$(ffmpeg -i ${TMPDIR}output.jpg 2>&1 |grep Video|awk '{ split( $8, pieces,  /[x,]/ ) ; print pieces[1] }')
-theheight=$(ffmpeg -i ${TMPDIR}output.jpg 2>&1 |grep Video|awk '{ split( $8, pieces,  /[x,]/ ) ; print pieces[2] }')
-theheight=`echo | awk '{printf  "%.0f ", theheight*SIZE/thewidth}' theheight="${theheight}}" SIZE="${SIZE}}" thewidth="${thewidth}"`
+thewidth=$($ffmpeg  -i ${TMPDIR}output.jpg 2>&1 |grep Video|awk '{ split( $8, pieces,  /[x,]/ ) ; print pieces[1] }')
+theheight=$($ffmpeg  -i ${TMPDIR}output.jpg 2>&1 |grep Video|awk '{ split( $8, pieces,  /[x,]/ ) ; print pieces[2] }')
+theheight=`echo | awk '{printf  "%.0f ", theheight*SIZE/thewidth}' theheight="${theheight}" SIZE="${SIZE}" thewidth="${thewidth}"`
 #theheight=$(echo "scale=0;$theheight*$SIZE/$thewidth" | bc)
-ffmpeg -i ${TMPDIR}output.jpg -vf scale=${SIZE}x${theheight} -vframes 1 ${TMPDIR}th.jpg
+$ffmpeg  -i ${TMPDIR}output.jpg -vf scale=${SIZE}x${theheight} -vframes 1 ${TMPDIR}th.jpg
 theheight=`echo | awk '{printf  "%.0f ", 150+theheight}' theheight="${theheight}}"`
 #theheight=$(echo "$theheight+150" | bc)
-ffmpeg -f lavfi -i color=white:${SIZE}x${theheight} -i ${TMPDIR}th.jpg -filter_complex "[0:v][1:v] overlay=0:150,drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:fontsize=20:fontcolor=black:x=30:y=20:textfile=${TMPDIR}myfile.txt" -vframes 1 th${RANDOM}.jpg
+$ffmpeg  -f lavfi -i color=white:${SIZE}x${theheight} -i ${TMPDIR}th.jpg -filter_complex "[0:v][1:v] overlay=0:150,drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:fontsize=20:fontcolor=black:x=30:y=20:textfile=${TMPDIR}myfile.txt" -vframes 1 th${RANDOM}.jpg
+echo $theheight
